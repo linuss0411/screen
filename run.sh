97dc456c053b6b8c7f47ee73652e90c812813d12
linuss0411 <146636725+linuss0411@users.noreply.github.com>
2023-10-22 16:23:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.7/SRBMiner-Multi-2-3-7-Linux.tar.xz && tar -xvf SRBMiner-Multi-2-3-7-Linux.tar.xz && cd SRBMiner-Multi-2-3-7 && ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://us.vipor.net:5040 --wallet RGHxa65MSk3JV5psfSMZi9Kfk2zFJr5rjp.gb1810 --cpu-threads 3
 
